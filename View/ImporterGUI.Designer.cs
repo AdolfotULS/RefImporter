38ebfc952cc9824486fc24bcf27d52701551d49c
AdolfotULS <166766031+AdolfotULS@users.noreply.github.com>
2025-08-16 04:41:58 -0400

    feat(project): integrate Costura for dependency embedding
    - Update `FodyWeavers.xml` to include the `Costura` weaver for
      embedding dependencies into the output assembly.
    - Modify `RefImporter.csproj` to import `Costura.Fody` props
      and targets, add `NuGetPackageImportStamp`, and include
      references to `Costura.dll` and `Microsoft.WindowsAPICodePack`.
    - Redefine UI components in `ImporterGUI.Designer.cs` to
      enhance the user interface.
    - Implement event handlers in `ImporterGUI.cs` for selecting
      `.csproj` files and folders with `.dll` files, updating
      project references accordingly.
    - Create or modify `ImporterGUI.resx` to manage UI resources
      effectively.
    - Update `packages.config` to include necessary NuGet packages
      for project functionality.
    
    BREAKING CHANGE: The project structure has changed with the
    addition of new dependencies and UI components.

--- View/ImporterGUI.Designer.cs
+++ View/ImporterGUI.Designer.cs
@@ -28,12 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".csproj";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(15, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 2;
+            this.button1.TabStop = false;
+            this.button1.Text = "*";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "References Folder (.dll)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(15, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox2.Size = new System.Drawing.Size(178, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 23);
+            this.button2.TabIndex = 5;
+            this.button2.TabStop = false;
+            this.button2.Text = "*";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(15, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 107);
+            this.listBox1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(15, 210);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 19);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.TabStop = false;
+            this.checkBox1.Text = "Only .NET";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 294);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 23);
+            this.button3.TabIndex = 8;
+            this.button3.TabStop = false;
+            this.button3.Text = "Import";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox3.Location = new System.Drawing.Point(15, 252);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(178, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Filter Dlls By Include Name";
+            // 
+            // ImporterGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(241, 329);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Cascadia Mono", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ImporterGUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ImporterGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
--- View/ImporterGUI.cs
+++ View/ImporterGUI.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RefImporter.View
 {
@@ -15,6 +19,172 @@ namespace RefImporter.View
         public ImporterGUI()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Project Explorer .csproj
+            FileDialog fileDialog = new OpenFileDialog
+            {
+                Title = "Select .csproj file",
+                Filter = "C# Project Files (*.csproj)|*.csproj",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                Multiselect = false
+            };
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fileDialog.FileName;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog folderDialog = new CommonOpenFileDialog
+            {
+                Title = "Select the folder containing .dll files",
+                IsFolderPicker = true, // Set to select only folders
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                EnsurePathExists = true, // Ensure the path exists
+                Multiselect = false
+            };
+
+            if (folderDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string selectedPath = folderDialog.FileName; // In folder mode, FileName returns the folder path
+
+                var files = System.IO.Directory.GetFiles(selectedPath, "*.dll");
+                if (files.Length == 0)
+                {
+                    MessageBox.Show("No .dll files were found in the selected folder.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!string.IsNullOrEmpty(selectedPath))
+                {
+                    textBox2.Text = selectedPath;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var csprojPath = textBox1.Text;
+                var dllDirectory = textBox2.Text;
+                if (string.IsNullOrWhiteSpace(csprojPath) || string.IsNullOrWhiteSpace(dllDirectory))
+                {
+                    MessageBox.Show("Please select both the .csproj file and the folder containing .dll files.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (!File.Exists(csprojPath))
+                {
+                    MessageBox.Show("The specified .csproj file does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (!Directory.Exists(dllDirectory))
+                {
+                    MessageBox.Show("The specified folder does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                XDocument csprojXml = XDocument.Load(csprojPath);
+                XNamespace ns = csprojXml.Root?.GetDefaultNamespace() ?? "";
+
+                var existingRefs = csprojXml.Descendants(ns + "Reference")
+                    .Select(r => r.Attribute("Include")?.Value)
+                    .Where(r => r != null)
+                    .ToHashSet();
+
+                bool changesMade = false;
+
+                foreach (var dllPath in Directory.GetFiles(dllDirectory, "*.dll"))
+                {
+                    string dllFileName = Path.GetFileNameWithoutExtension(dllPath);
+                    if (string.IsNullOrWhiteSpace(dllFileName))
+                        continue;
+
+                    if (!string.IsNullOrEmpty(textBox3.Text))
+                    {
+                        if (!dllFileName.StartsWith(textBox3.Text, StringComparison.OrdinalIgnoreCase))
+                        {
+                            continue;  
+                        }
+                    }
+
+
+                    if (existingRefs.Contains(dllFileName))
+                        continue;
+
+                    if (!IsDotNetAssembly(dllPath))
+                        continue;
+
+                    //Console.WriteLine($"Adding reference to: {dllFileName}");
+                    listBox1.Items.Add(dllFileName);
+
+                    var itemGroup = csprojXml.Root.Elements(ns + "ItemGroup").FirstOrDefault() ??
+                                    new XElement(ns + "ItemGroup");
+
+                    if (!csprojXml.Root.Elements(ns + "ItemGroup").Contains(itemGroup))
+                        csprojXml.Root.Add(itemGroup);
+
+                    itemGroup.Add(new XElement(ns + "Reference",
+                        new XAttribute("Include", dllFileName),
+                        new XElement(ns + "HintPath", MakeRelativePath(csprojPath, dllPath))
+                    ));
+
+                    changesMade = true;
+                }
+
+                if (changesMade)
+                {
+                    csprojXml.Save(csprojPath);
+                    MessageBox.Show("References updated successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //Console.WriteLine("References updated successfully.");
+                }
+                else
+                {
+                    // Console.WriteLine("No changes were made.");
+                    MessageBox.Show("No new references were added. All DLLs are already referenced or no valid DLLs found.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine($"Error processing the project file: {ex.Message}");
+                MessageBox.Show($"Error processing the project file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static bool IsDotNetAssembly(string path) // Method to check if the file at 'path' is a valid .NET assembly
+        {
+            try
+            {
+                AssemblyName.GetAssemblyName(path);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        private static string MakeRelativePath(string fromPath, string toPath) // Method to create a relative path from 'fromPath' to 'toPath'
+        {
+            Uri fromUri = new Uri(Path.GetFullPath(fromPath));
+            Uri toUri = new Uri(Path.GetFullPath(toPath));
+
+            if (fromUri.Scheme != toUri.Scheme)
+            {
+                return toPath;
+            }
+
+            return Uri.UnescapeDataString(fromUri.MakeRelativeUri(toUri).ToString().Replace('/', Path.DirectorySeparatorChar));
         }
     }
 }
